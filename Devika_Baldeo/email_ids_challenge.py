c29964957fd44d31124076111ddcbb615be5f39b
DevikaBa <devikabaldeo@gmail.com>
2021-02-15 19:03:54 -0500

    completed the email challenge, with lots of help

--- Devika_Baldeo/email_ids_challenge.py
+++ Devika_Baldeo/email_ids_challenge.py
@@ -8,14 +8,20 @@ employee_name = 'Ash Rahman'
 # Let's write some code that converts a name into an email id that matches this format
 # 1.1 TODO: Let's save the lowercase version of the employee_name in a new variable 'lower_name'
 
+lower_name = employee_name.lower() 
+print(lower_name)
+
 # 1.2 TODO: We want to separate the first name and last name and save it in a variable 'names_list'
-
+names_list = lower_name.split()
+print(names_list)
 # 1.3 TODO: We want to join the first name and last name with a '.' and save it in a variable called 'joined_names'
+joined_names = ".".join(names_list)
+print(joined_names)
 
-# 1.4 TODO: We want to add '@ripplemedia.com' to the end of the string inside joined_names and save it in a variable 'email'
-# 
+# 1.4 TODO: We want to add  to the end of the string inside joined_names and save it in a variable 'email'
+email = joined_names + '@ripplemedia.com'
+print(email) 
 
-print()
 
 print('Question 2')
 
@@ -23,8 +29,22 @@ print('Question 2')
 names = ['Max Bartlett', 'Angelita Norris', 'Stewart Mueller', 'Dominique Henry', 'Carmela Gross', 'Bettie Mcmillan', 'Sara Ellison', 'Ira Anthony', 'Pauline Riley', 'Ben Weber',
          'Joanne Mcknight', 'Loren Gould', 'Jamar Singh', 'Amanda Vance', 'Tyrell Andrade', 'Jana Clements', 'Eddy Mcbride', 'Marsha Meyer', 'Elbert Shannon', 'Alyce Hull']
 emails = []
+for name in names:
+    # print(name)
+    lower_name = name.lower()
+    # print(lower_name)
+    names_list = lower_name.split()
+    # print(names_list)
+    joined_names = ".".join(names_list)
+    # print(joined_names)
+    email = joined_names + '@ripplemedia.com'
+    # print(email) 
+    emails.append(email)
+print(emails)
+
 
 # We want to convert all their names into the same format from Question 1
 # 2.1 TODO: Use a "for" loop to go over each name in the names list
+
 # 2.2 TODO: Inside the "for" loop, create the email id by re-using the logic from Question 1 and...
 # 2.3 TODO: ..add the email to the emails list
